98ae68a5f20614b7fdb25cddf92e7e1df5f9255f
Ivan <ab09@inbox.ru>
2022-10-12 15:43:47 +0500

    рисование треугольника

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 20,
+    xc = 80, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+
+int count = 10;
+
+while (count < 10)
+{
+    int what = new Random().Next(0, 3);
+    
+}
